26aabf65476319e713aefb0b9a1d27eba423d9e5
Christian Castro <xxchristxx1994@gmail.com>
2018-09-04 22:32:06 -0500

    Enter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Usuario } from './models/usuario';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Ticket Show';
+  public user: Usuario;
+  public identity;
+  public token;
+
+  constructor(){
+    this.user = new Usuario('','','','');
+  }
+  public onSubmit(){
+    console.log(this.user);
+  }
 }
